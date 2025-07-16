3b90ea4d4a533ba6138e77802741ac8cd0d9949a
NITHESH-24 <nitheshsuresh381@gmail.com>
2025-07-16 09:17:12 +0530

    Add files via upload

---
+++ Dat-11 Task/CalculatorTest.java
@@ -0,0 +1,30 @@
+package com.training.demo;
+
+import com.training.demo.controllers.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    Calculator ca = new Calculator();
+
+    @Test
+    public void testAddition(){
+        assertEquals(5,ca.add(2,3));
+    }
+
+    @Test
+    public void testSub(){
+        assertEquals(1,ca.sub(3,2));
+    }
+
+    @Test
+    public void testMul(){
+        assertEquals(6,ca.mul(2,3));
+    }
+
+    @Test
+    public void testDiv(){
+        assertEquals(2,ca.div(6,3));
+    }
+}
